6cc358e4358e16a1dfb3b7fd5bb683c1eeebdf8d
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-09-05 16:31:38 -0700

    add id on response

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -3,17 +3,21 @@ from pydantic import BaseModel
 # This is API related
 class Blog(BaseModel):
     title:str
-    body: str
-    
-class BlogResponse(BaseModel):
-    title:str
-    body: str
-    
+    body: str    
 class User(BaseModel):
     name:str
     username:str
     password:str
 
 class UserResponse(BaseModel):
+    id:int
     name:str
     username:str
+
+    
+class BlogResponse(BaseModel):
+    id:int
+    title:str
+    body: str
+    creator:UserResponse
+    
