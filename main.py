f3834e9e8fe83fa8d4b19366dbd9a7705ff72ab1
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-09-05 14:53:58 -0700

    blog update

--- main.py
+++ main.py
@@ -35,9 +35,20 @@ def create_blog(request:schemas.Blog,db:Session=Depends(get_db)):
 
 @app.get('/api/blogs/{id}',status_code=status.HTTP_200_OK)
 def get_blog(id,db:Session=Depends(get_db)):
-    blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    blog=db.query(models.Blog).get(id)
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Blog not found")
+    return blog
+
+@app.put('/api/blogs/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id,request: schemas.Blog,db:Session=Depends(get_db)):
+    blog=db.query(models.Blog).get(id)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Blog not found")
+    blog.title=request.title
+    blog.body=request.body
+    db.commit()
+    db.refresh(blog)
     return blog
 
 @app.delete('/api/blogs/{id}', status_code=status.HTTP_204_NO_CONTENT)
