9fbc5a6f43f56c3e6e8ec61fadd444efba11f7eb
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-09-05 11:56:07 -0700

    api title

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-app=FastAPI()
+app=FastAPI(
+    docs_url="/",
+    title="Blog Fast API Documentation",
+    description="Blog Fast API documentation"
+)
 
-@app.get('/api/')
-def get():
+@app.get('/api/blogs')
+def get_all_blogs():
     return {"message":"Hello, world!"}
