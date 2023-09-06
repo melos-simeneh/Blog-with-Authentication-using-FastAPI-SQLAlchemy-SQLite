9e73329fe2af36af14097a2b520e6e96c9cf506f
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-09-06 10:57:15 -0700

    access token

--- controllers/auth.py
+++ controllers/auth.py
@@ -3,16 +3,15 @@ from sqlalchemy.orm import Session
 from models import models
 from schemas import schemas
 from utils.bcrypt import Hash
+from utils.jwt import create_access_toekn
 
 def login(request: schemas.Login,db: Session):
     user=db.query(models.User).filter(models.User.username==request.username).first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    if not Hash.verify_password(request.password,user.password):
-         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Incorrect username or password",)
-    
-    return "Correct"
+    if not user or not Hash.verify_password(request.password,user.password):
+         raise HTTPException(
+             status_code=status.HTTP_404_NOT_FOUND,
+             detail="Incorrect username or password",
+             headers={"WWW-Authenticate": "Bearer"}
+             )
+    access_token=create_access_toekn(data={"sub":user.username})
+    return {"access_token":access_token,"token_type":"bearer"}
